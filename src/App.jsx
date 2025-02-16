8ba06b15ccf42ab1c08a617e07f4dd35782bca70
Shash <ashishsinghlogic@gmail.com>
2025-02-16 17:27:18 +0530

    widget fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div>App hai </div>
+    <div>App  </div>
   )
 }
 
--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+
+
 export default function Widget() {
 
     const [timeLeft, setTimeLeft] = useState(0);
+    const [totalWords, setTotalWords] = useState(0);
+
     // will add logic here 
 
-    UseEffect(() => {
+    useEffect(() => {
         const visibleText = () => {
             //document.createTreeWalker is a built-in browser API
             // that creates a TreeWalker object, which allows you to traverse the DOM tree
 
             //its preffered over HTML.innerText and querySelector because the traversal is easy and 
             // in this case we need only visible text on the web page.
-            document.createTreeWalker(
-                document.body
-            NodeFilter.SHOW_TEXT
-            )
+            const walker = document.createTreeWalker(
+                document.body,
+                NodeFilter.SHOW_TEXT
+            );
 
 
             let visibleText = '';
@@ -32,7 +36,7 @@ export default function Widget() {
                 }
 
                 // If the parent element is visible, append the text to the visibleText string, followed by a space (to separate words).
-            };
+            }
 
             return visibleText.trim();
         };
@@ -47,16 +51,24 @@ export default function Widget() {
             );
         };
 
-        const words = getVisibleText().split(/\s+/)
+        const words = VisibleText().split(/\s+/);
         setTotalWords(words.length)
+
     }, []);
+
+    //dynamic time calculation 
+
+
+
+    const BASE_WP = 200;
+    // average reading time in english
 
 
 
     return (
 
         <div className="fixed bottom-4 right-4 bg-black/80 text-white p-3 rounded-lg shadow-lg">
-            ⏳ {timeRemaining}m remaining
+            ⏳ {timeLeft}m remaining | Words: {totalWords}
         </div>
 
     )
